258f0c74eda5e7d49aad281953977636ba861072
Marian Jureczko <marian.jureczko@gmail.com>
2024-08-24 21:40:13 +0200

    mutable state

---
+++ app/src/main/java/pl/marianjureczko/sharedviewmodel/MainActivity.kt
@@ -0,0 +1,62 @@
+package pl.marianjureczko.sharedviewmodel
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                MaterialTheme(
+                    colors = lightColors(),
+                    content = {
+                        var name = mutableStateOf("")
+                        Column {
+                            val name1 = name.value
+                            Greeting(name1)
+                            NameInput(name = name.value, onValueChange = {
+                                name.value = it
+                            })
+                        }
+                    }
+                )
+        }
+    }
+}
+
+@Composable
+fun NameInput(name: String, onValueChange: (String) -> Unit) {
+
+    TextField(
+        value = name,
+        onValueChange = onValueChange,
+        label = { Text("Name") }
+    )
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+        Greeting("Android")
+}
